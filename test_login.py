faf7158b4a7c06aa3930d51a92cf3a1683230e7a
resaba17 <resaba723@gmail.com>
2026-06-12 13:28:23 +0530

    Update login credentials in test_login.py

--- test_login.py
+++ test_login.py
@@ -2,6 +2,6 @@ from pages.login_page import LoginPage
 def test_successful_login(page):
     login = LoginPage(page)
     login.open_login_page()
-    login.login("resaba723@gmail.com", "Resaba9#")
+    login.login("YOUR_EMAIL","YOUR_PASSWORD")
     page.wait_for_timeout(5000)
     assert "dashboard"in page.url.lower() 
