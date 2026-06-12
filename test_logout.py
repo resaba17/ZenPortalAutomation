2779cd7f3b2769d0ef5b18f07f5676598077b4a1
resaba17 <resaba723@gmail.com>
2026-06-12 13:29:13 +0530

    Modify test_logout to use generic credentials
    Updated test_logout to use placeholder email and password.

--- test_logout.py
+++ test_logout.py
@@ -5,7 +5,7 @@ def test_logout(page):
     login = LoginPage(page)
     dashboard = DashboardPage(page)
     login.open_login_page()
-    login.login("resaba723@gmail.com", "Resaba9#")
+    login.login("YOUR_EMAIL",YOUR_PASSWORD")
     page.wait_for_timeout(5000)
 
     #dashboard.close_popup()
